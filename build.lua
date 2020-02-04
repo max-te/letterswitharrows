c0a5ccc6473fbc9af04b696b58a8b173148045e4
Max Teegen <870074+max-te@users.noreply.github.com>
2020-02-04 16:57:10 +0100

    Add tagging

--- build.lua
+++ build.lua
@@ -2,3 +2,12 @@ module = "letterswitharrows"
 
 unpackfiles = {"*.dtx"}
 ctanreadme = "README.txt"
+
+function update_tag(file,content,tagname,tagdate)
+    if string.match(file, "%.dtx$") then
+        return string.gsub(content,
+            "\n%% \\date{Released %d%d%d%d[-/]%d%d[-/]%d%d}\n",
+            "\n%% \\date{Released " .. tagdate .. "}\n")
+    end
+    return content
+end
